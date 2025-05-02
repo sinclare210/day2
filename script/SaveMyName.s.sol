a96145ad3107b4f178e3e1b4e76b409f57525154
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-02 21:21:01 +0100

    done

---
+++ script/SaveMyName.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {Script, console} from "forge-std/Script.sol";
+import {SaveMyName} from "../src/SaveMyName.sol";
+
+
+contract SaveMyNameScript is Script {
+
+    SaveMyName public saveMyName;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        saveMyName = new SaveMyName();
+        vm.stopBroadcast();
+    }
+}
---
+++ test/SaveMyName.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {SaveMyName} from "../src/SaveMyName.sol";
+
+contract SaveMyNameTest is Test {
+    string public name;
+    string public bio;
+    SaveMyName public saveMyName;
+
+    function setUp () public {
+        name = "Sinclair";
+        bio = "Dapps";
+        saveMyName = new SaveMyName();
+    }
+
+    function testFunc () public view {
+        assertEq(name, "Sinclair");
+        assertEq(bio, "Dapps");
+    }
+
+    function testAddandRetrieve () public {
+        saveMyName = new SaveMyName();
+        saveMyName.add("Frank", "Married");
+
+        (string memory savedName, string memory savedBio) = saveMyName.retreive();
+
+        assertEq(savedName, "Frank");
+        assertEq(savedBio, "Married");
+
+    }
+
+}
